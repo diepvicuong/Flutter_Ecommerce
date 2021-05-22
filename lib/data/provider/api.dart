e208f5ef9e8215bbc593d8eac34c2df2854e7b61
diepvicuong <diepvicuong97@gmail.com>
2021-05-22 16:49:47 +0700

    add dio_http_cache

--- lib/data/provider/api.dart
+++ lib/data/provider/api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:nordic_ecommerce/data/models/home/home_model.dart';
 import 'package:nordic_ecommerce/data/models/product/product_list_model.dart';
 import 'package:nordic_ecommerce/data/models/product/product_model.dart';
@@ -8,13 +9,19 @@ import 'package:nordic_ecommerce/res/config.dart';
 
 class MyApiClient {
   final Dio httpClient;
-
-  MyApiClient({required this.httpClient});
+  late DioCacheManager _dioCacheManager;
+  Options _cacheOptions =
+      buildCacheOptions(Duration(days: 7), forceRefresh: true);
+  MyApiClient({required this.httpClient}) {
+    _dioCacheManager = DioCacheManager(CacheConfig());
+    this.httpClient.interceptors.add(_dioCacheManager.interceptor);
+  }
 
   fetchAll() async {
     HomeModel? homeModel;
     try {
-      var response = await httpClient.get(AppConfig.homeUrl);
+      var response =
+          await httpClient.get(AppConfig.homeUrl, options: _cacheOptions);
 
       if (response.statusCode == 200 && response.data['data'] != null) {
         homeModel = HomeModel.fromJson(response.data['data']);
@@ -31,7 +38,8 @@ class MyApiClient {
     ProductDataList? productDataList;
     try {
       print(AppConfig.productListUrl(categoryId));
-      var response = await httpClient.get(AppConfig.productListUrl(categoryId));
+      var response = await httpClient.get(AppConfig.productListUrl(categoryId),
+          options: _cacheOptions);
 
       if (response.statusCode == 200 && response.data['data'] != null) {
         productDataList = ProductDataList.fromJson(response.data['data']);
@@ -47,8 +55,8 @@ class MyApiClient {
     Product? product;
     try {
       print(AppConfig.productListUrl(productId));
-      var response =
-          await httpClient.get(AppConfig.productDetailUrl(productId));
+      var response = await httpClient.get(AppConfig.productDetailUrl(productId),
+          options: _cacheOptions);
 
       if (response.statusCode == 200 && response.data['data'] != null) {
         product = Product.fromJson(response.data['data']);
